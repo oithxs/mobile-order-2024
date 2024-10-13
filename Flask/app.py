deebab9d2528e85f2a50351dc5310d32d933a5df
masaki0087 <136552609+masaki0087@users.noreply.github.com>
2024-10-13 12:33:46 +0900

    ページのコメントの追加

--- Flask/app.py
+++ Flask/app.py
@@ -6,22 +6,27 @@ app = Flask(__name__)
 def home():
     return "Welcome to Flask!!!"
 
+#トップページ
 @app.route('/top')
 def top():
     return render_template("/user/top.html")
 
+#お客さんが注文するページ
 @app.route('/order')
 def order():
     return render_template("/user/order.html")
 
+#注文完了のページ
 @app.route('/result',methods=['GET'])
 def result():
     return render_template("/user/result.html")
 
+#注文履歴のページ
 @app.route('/history')
 def history():
     return render_template("/user/history.html")
 
+#/adminの閲覧ページ
 @app.route('/admin')
 def admin():
     return render_template("/admin/manage.html")
