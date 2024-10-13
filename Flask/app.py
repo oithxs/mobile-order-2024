5e9aaf1e493ea51e664a14416b052461ae5fc67b
masaki0087 <136552609+masaki0087@users.noreply.github.com>
2024-10-13 12:30:04 +0900

    app.pyのルーティング作成

--- Flask/app.py
+++ Flask/app.py
@@ -1,7 +1,27 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/',methods=['GET'])
 def home():
     return "Welcome to Flask!!!"
+
+@app.route('/top')
+def top():
+    return render_template("/user/top.html")
+
+@app.route('/order')
+def order():
+    return render_template("/user/order.html")
+
+@app.route('/result',methods=['GET'])
+def result():
+    return render_template("/user/result.html")
+
+@app.route('/history')
+def history():
+    return render_template("/user/history.html")
+
+@app.route('/admin')
+def admin():
+    return render_template("/admin/manage.html")
