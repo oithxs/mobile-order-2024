8ceb27ad009056f1cf8f0dffde4cc7e5852daf9c
dice-2004 <hd.daisuke@icloud.com>
2024-10-22 20:43:34 +0900

    どうにか頑張ってみましたよ

--- Flask/app.py
+++ Flask/app.py
@@ -1,21 +1,91 @@
 
 from flask import Flask, render_template
-from flask_sqlalchemy import SQLAlchemy
+
 import os
-from dotenv import load_dotenv
+# import pymysql
 
 # envファイルを読み込む
+
+
+app = Flask(__name__)
+
+
+# def create_superuser(host, user, password):
+#     connection = pymysql.connect(
+#     host=host,
+#     user=user,
+#     password=password,
+#     cursorclass=pymysql.cursors.DictCursor
+#     )
+
+#     try:
+#         with connection.cursor() as cursor:
+#             # 新しいユーザを作成
+#             new_user = 'new_user'
+#             new_password = 'new_password'
+#             cursor.execute(f"CREATE USER '{new_user}'@'%' IDENTIFIED BY '{new_password}';")
+
+#             # ユーザに権限を付与
+#             cursor.execute(f"GRANT ALL PRIVILEGES ON *.* TO '{new_user}'@'%';")
+
+#             # 権限の変更を反映
+#             cursor.execute("FLUSH PRIVILEGES;")
+
+#         # コミット
+#         connection.commit()
+
+#     finally:
+#         connection.close()
+
+
+
+
+
+
+
+def show_nickname(data):
+    for dt in data:
+        print(f"{dt.id} {dt.name} {dt.status}")
+
+def show_reservation(data):
+    for dt in data:
+        print(f"{dt.id} {dt.name} {dt.number} {dt.ketchup} {dt.mustard} {dt.reservationTime}")
+
+@app.route('/')
+def top():
+
+    show_nickname(get_nicknames())
+
+    update_nickname_status(Nickname,2,True)
+
+    add_reservation("枚方太郎",10,True,False,datetime.now())
+
+    show_reservation(get_reservations())
+
+    update_reservation_by_id(1,"紺扉宇太",120,False,False,datetime.now())
+
+    return 'ok'
+
+if __name__ == '__main__':
+    app.debug = True
+    # create_superuser(host, user, password)
+    app.run()
+
+
+from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
+import os
 load_dotenv()
 user = os.environ['USER']
 password = os.environ['PASSWORD']
 db = os.environ['DB']
 host = os.environ['HOST']
 
-app = Flask(__name__)
-
 db_uri = f'mysql+pymysql://{user}:{password}@{host}/{db}?charset=utf8'
 app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
 db = SQLAlchemy(app)
+
+
 # ---------------------------------------------------------------------------
 # DB生成処理
 class Reservation(db.Model):
@@ -26,7 +96,7 @@ class Reservation(db.Model):
     ketchup = db.Column(db.Boolean)
     mustard = db.Column(db.Boolean)
     reservationTime = db.Column(db.DateTime)
-    
+
 class Nickname(db.Model):
     __tablename__ = 'nickname'
     id = db.Column(db.Integer,primary_key=True,autoincrement=True)
@@ -40,7 +110,7 @@ class Received(db.Model):
     number = db.Column(db.Boolean)
 # ---------------------------------------------------------------------------
 # adminの削除処理
-@app.route('/destroy/<int:id>')
+
 def destroy(id):
     message = "Destroy SQLAlchemy"
 
@@ -50,6 +120,7 @@ def destroy(id):
     db.session.commit()
 
     return 'ok'
+
 # ---------------------------------------------------------------------------
 # /adminと/userの取得処理
 from datetime import datetime
@@ -80,11 +151,12 @@ def add_reservation(name: str, number: int, ketchup: bool, mustard: bool, now_ti
 def get_reservations():
     reservations = Reservation.query.all()
     return reservations
+
 # ---------------------------------------------------------------------------
 # /adminの編集処理
 def update_reservation_by_id(id: int, name: str, number: int, ketchup: bool, mustard: bool, now_time: datetime):
     reservation = Reservation.query.get(id)
-    
+
     if reservation:
         reservation.name = name
         reservation.number = number
@@ -92,36 +164,7 @@ def update_reservation_by_id(id: int, name: str, number: int, ketchup: bool, mus
         reservation.mustard = mustard
         reservation.time = now_time
         db.session.commit()
-        
+
     else:
         print("Reservation not found.")
 
-
-# ---------------------------------------------------------------------------
-# デバッグ用プログラム
-def show_nickname(data):
-    for dt in data:
-        print(f"{dt.id} {dt.name} {dt.status}")
-
-def show_reservation(data):
-    for dt in data:
-        print(f"{dt.id} {dt.name} {dt.number} {dt.ketchup} {dt.mustard} {dt.reservationTime}")
-
-@app.route('/')
-def top():
-
-    show_nickname(get_nicknames())
-
-    update_nickname_status(Nickname,2,True)
-
-    add_reservation("枚方太郎",10,True,False,datetime.now())
-
-    show_reservation(get_reservations())
-
-    update_reservation_by_id(1,"紺扉宇太",120,False,False,datetime.now())
-
-    return 'ok'
-
-if __name__ == '__main__':
-    app.debug = True
-    app.run()
