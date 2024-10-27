d7004fa2523703c7730af2faa718a869aa2541ef
masaki0087 <136552609+masaki0087@users.noreply.github.com>
2024-10-27 19:02:38 +0900

    予約時刻のパディング関数と合計金額を求める関数の追加

--- Flask/static/js/user/order.js
+++ Flask/static/js/user/order.js
@@ -1,8 +1,28 @@
 window.onload = function () {
 
+    let paddingTime = function(str) {
+        if(str.length == 1){
+            str = "0"+ str;
+        }
+        return str;
+    }
+
     // 現在の時刻を取得し入力欄に代入
     const date = new Date();
     rTime = document.getElementById("reservationTime");
     console.log(rTime.value);
-    rTime.value = date.getHours().toString() + ":" + date.getMinutes().toString();
+    rTime.value = date.getHours().toString() + ":" + paddingTime(date.getMinutes().toString());
+
+
+    let calcSum = function (value) {
+        return 150 * value;
+    }
+
+    count = document.getElementById("count");
+
+    count.addEventListener("change",function(){
+        money = document.getElementById("resultMoney");
+        money.innerText = calcSum(count.value);
+        }
+    )
 };
